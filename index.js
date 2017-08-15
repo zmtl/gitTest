07ae35d410383ce46e365fc8b44e5998be0a890b
hunk <zhangmin_hunk@163.com>
2017-08-15 21:24:36 +0800

    merge test

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 function f1(){
 	console.log(121)
 }
-=======
-function f2(){
-	console.log(3)
-}
-var box1 = document.getElementById("box1");
->>>>>>> aa5995ef8fd57d2a649c7f685620e43dadaff018
